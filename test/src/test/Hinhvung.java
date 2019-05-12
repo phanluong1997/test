3950ae7b8318ee9102074d003748154d101b8bf4
thnhanedu <50509322+thnhanedu@users.noreply.github.com>
2019-05-12 20:10:00 +0700

    New

--- test/src/test/Hinhvung.java
+++ test/src/test/Hinhvung.java
@@ -3,6 +3,10 @@ package test;
 public class Hinhvung {
 	private int canh = 0;
 	
+	public Hinhvung(int canh) {
+		this.canh = canh;
+	}
+	
 	public long layDientich() {
 		return this.canh * this.canh;
 	}
